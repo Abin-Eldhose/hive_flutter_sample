a75c21ee77ed6313df60bedc17e9671b41070147
mirshab001 <mirshabpm01@gmail.com>
2025-03-26 15:41:18 +0530

    changed

---
+++ lib/screens/todo/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Text("HELLO WORLD"),
+      ),
+    );
+  }
+}
